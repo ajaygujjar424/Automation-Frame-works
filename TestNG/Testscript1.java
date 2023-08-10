d0ba2c2c6f3b992b69e2c8a18cb39e68a0bce4d9
Ajay Gujjar <127547339+ajaygujjar424@users.noreply.github.com>
2023-08-10 18:20:45 +0530

    Add files via upload
    TestNg Framework

---
+++ TestNG/Testscript1.java
@@ -0,0 +1,16 @@
+package generic;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Testscript1 extends Generic
+{
+  @Test
+  public void login()
+  {
+	  driver.findElement(By.id("email")).sendKeys("ajaygujjar424@gmail.com");
+	  driver.findElement(By.name("pass")).sendKeys("ajay1999");
+	  driver.findElement(By.xpath("//button[@type='submit']")).click();
+	  
+  }
+}
